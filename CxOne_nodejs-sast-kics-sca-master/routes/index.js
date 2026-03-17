a1b588746792b60893f700fefa0ff84b7c550f08
Andrii Radishevskyi <191871656+cx-andrii-radishevskyi@users.noreply.github.com>
2026-03-17 15:06:48 +0200

    Checkmarx Bot - Auto Pull Request

--- CxOne_nodejs-sast-kics-sca-master/routes/index.js
+++ CxOne_nodejs-sast-kics-sca-master/routes/index.js
@@ -8,7 +8,7 @@ var ms = require('ms');
 var streamBuffers = require('stream-buffers');
 var readline = require('readline');
 var moment = require('moment');
-var exec = require('child_process').exec;
+var execFile = require('child_process').execFile;
 var validator = require('validator');
 
 // zip-slip
@@ -156,14 +156,22 @@ exports.create = function (req, res, next) {
   var imgRegex = /\!\[alt text\]\((http.*)\s\".*/;
   if (typeof (item) == 'string' && item.match(imgRegex)) {
     var url = item.match(imgRegex)[1];
-    console.log('found img: ' + url);
 
-    exec('identify ' + url, function (err, stdout, stderr) {
-      console.log(err);
-      if (err !== null) {
-        console.log('Error (' + err + '):' + stderr);
-      }
-    });
+    // Validate URL to prevent command injection
+    if (validator.isURL(url, { protocols: ['http', 'https'], require_protocol: true })) {
+      console.log('found img: ' + url);
+
+      // Use execFile instead of exec to prevent command injection
+      // execFile does not spawn a shell and treats arguments as separate parameters
+      execFile('identify', [url], function (err, stdout, stderr) {
+        console.log(err);
+        if (err !== null) {
+          console.log('Error (' + err + '):' + stderr);
+        }
+      });
+    } else {
+      console.log('Invalid URL detected, skipping image processing');
+    }
 
   } else {
     item = parse(item);
