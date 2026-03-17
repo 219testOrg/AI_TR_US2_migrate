5b29a39fd31d5a38fdf61a17bd66fe3d7e3736b2
Andrii Radishevskyi <191871656+cx-andrii-radishevskyi@users.noreply.github.com>
2026-03-17 15:07:44 +0200

    Checkmarx Bot - Auto Pull Request

--- CxOne_nodejs-sast-kics-sca-master/routes/index.js
+++ CxOne_nodejs-sast-kics-sca-master/routes/index.js
@@ -9,6 +9,7 @@ var streamBuffers = require('stream-buffers');
 var readline = require('readline');
 var moment = require('moment');
 var exec = require('child_process').exec;
+var execFile = require('child_process').execFile;
 var validator = require('validator');
 
 // zip-slip
@@ -158,12 +159,19 @@ exports.create = function (req, res, next) {
     var url = item.match(imgRegex)[1];
     console.log('found img: ' + url);
 
-    exec('identify ' + url, function (err, stdout, stderr) {
-      console.log(err);
-      if (err !== null) {
-        console.log('Error (' + err + '):' + stderr);
-      }
-    });
+    // Validate URL format to ensure it's a valid URL
+    if (validator.isURL(url, { protocols: ['http', 'https'], require_protocol: true })) {
+      // Use execFile instead of exec to prevent command injection
+      // execFile does not spawn a shell, so special shell characters are treated as literals
+      execFile('identify', [url], function (err, stdout, stderr) {
+        console.log(err);
+        if (err !== null) {
+          console.log('Error (' + err + '):' + stderr);
+        }
+      });
+    } else {
+      console.log('Invalid URL format: ' + url);
+    }
 
   } else {
     item = parse(item);
